cc46be57ace53c99b110e8292c0e9138c7c48c58
infernion <sergiykhalimon@gmail.com>
2013-11-03 01:50:10 +0200

    some modify settings.py

--- friends_on_map/settings.py
+++ friends_on_map/settings.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
-import os
+import os.path
 import dj_database_url
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    # ('Sergiy Khalymon', 'sergiykhalimon@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -14,6 +16,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': dj_database_url.config(default='sqlite:///db.sqlite')
 }
+
+### For Postgres
+# DATABASES['default'] =  dj_database_url.config()
 
 # DATABASES = {
 #     'default': {
@@ -29,13 +34,13 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Kiev'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -67,17 +72,12 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
 
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -114,9 +114,7 @@ ROOT_URLCONF = 'friends_on_map.urls'
 WSGI_APPLICATION = 'friends_on_map.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (

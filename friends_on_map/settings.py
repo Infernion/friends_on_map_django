205eb60c5e250be8b62cae41102ab289eff22e8b
infernion <sergiykhalimon@gmail.com>
2013-11-03 12:43:55 +0200

    fix settings

--- friends_on_map/settings.py
+++ friends_on_map/settings.py
@@ -190,12 +190,19 @@ SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.social_auth.auth_allowed',
     'social.pipeline.social_auth.social_user',
     'social.pipeline.user.get_username',
-    'example.app.pipeline.require_email',
+    'auth.app.pipeline.require_email',
     'social.pipeline.mail.mail_validation',
     'social.pipeline.user.create_user',
     'social.pipeline.social_auth.associate_user',
     'social.pipeline.social_auth.load_extra_data',
     'social.pipeline.user.user_details'
+)
+
+SOCIAL_AUTH_DISCONNECT_PIPELINE = (
+    'social.pipeline.disconnect.allowed_to_disconnect',
+    'social.pipeline.disconnect.get_entries',
+    'social.pipeline.disconnect.revoke_tokens',
+    'social.pipeline.disconnect.disconnect'
 )
 
 SOCIAL_AUTH_FACEBOOK_KEY = '223307271163808'

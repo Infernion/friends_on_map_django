432198c5f5ef6fbed773feae6f38be9cbebd31cc
infernion <sergiykhalimon@gmail.com>
2013-11-09 23:46:20 +0200

    play with model.py

--- auth/models.py
+++ auth/models.py
@@ -1,28 +1,23 @@
+"""Django ORM models for Social Auth"""
+import base64
+import six
+
 from social.storage.base import UserMixin, AssociationMixin, NonceMixin, \
                                 CodeMixin, BaseStorage
-from urllib import urlopen
 
-class UserModel(UserMixin):
-	def add_friends(self):
-		# friends = 'https://api.vk.com/method/friends.get?uid=%s&cids=%s&fields=%s&access_token=%s' % (self.u)
-		self.extra_data['test'] = 'test'
 
-class NonceModel(NonceMixin):
-	pass
+class DjangoUserMixin(UserMixin):
+    pass
 
-class AssociationModel(AssociationMixin):
-	pass
+class DjangoNonceMixin(NonceMixin):
+    pass
 
-class CodeModel(CodeMixin):
-	pass
+class DjangoCodeMixin(CodeMixin):
+    pass
 
-class StorageImlpementation(BaseStorage):
-    user = UserModel
-    nonce = NonceModel
-    association = AssociationModel
-    code = CodeModel
-    
-    @classmethod
-    def is_integrity_error(cls, exception):
-        """Check if given exception flags an integrity error in the DB"""
-        raise NotImplementedError('Implement in subclass')
+class BaseDjangoStorage(BaseStorage):
+    user = DjangoUserMixin
+    code = DjangoCodeMixin
+
+user = DjangoUserMixin()
+user.set_extra_data(extra_data={'test':'test2'})

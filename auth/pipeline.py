a82b97563d384e8f128b0a9d58d24d21f4541b73
infernion <sergiykhalimon@gmail.com>
2013-11-16 23:18:48 +0200

    add pipline to get avatar #4

--- auth/pipeline.py
+++ auth/pipeline.py
@@ -4,14 +4,7 @@ def get_user_avatar(backend, details, response, social_user, uid,\
     if backend.__class__ == FacebookBackend:
         url = "http://graph.facebook.com/%s/picture?type=large" % response['id']
  
-    elif backend.__class__ == TwitterBackend:
-        url = response.get('profile_image_url', '').replace('_normal', '')
- 
     if url:
-        profile = user.get_profile()
-        avatar = urlopen(url).read()
-        fout = open(filepath, "wb") #filepath is where to save the image
-        fout.write(avatar)
-        fout.close()
-        profile.photo = url_to_image # depends on where you saved it
-        profile.save()
+        avatar = url
+        social_user.set_extra_data({'photo': url})
+        social_user.save()

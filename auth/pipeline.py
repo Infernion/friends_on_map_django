83cc9ccfae9b939c4f1d404c432dcabad115d4a8
infernion <sergiykhalimon@gmail.com>
2013-11-17 01:00:39 +0200

    try to get avatar #4

--- auth/pipeline.py
+++ auth/pipeline.py
@@ -3,6 +3,6 @@ from django.core.files.base import ContentFile
 
 def save_picture(strategy, user, response, details, social_user, is_new=False, *args, **kwargs):
 
-    if is_new and strategy.backend.name == 'facebook':
-        url = 'http://graph.facebook.com/{0}/picture'.format(response['id'])
+    #if is_new and strategy.backend.name == 'facebook':
+    url = 'http://graph.facebook.com/{0}/picture'.format(response['id'])
     social_user.set_extra_data('url':url)

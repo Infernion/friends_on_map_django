89fb60b05661b1214952178c37ca3b431f81c24e
infernion <sergiykhalimon@gmail.com>
2013-11-17 14:38:11 +0200

    try to get fb friends #1

--- auth/pipeline.py
+++ auth/pipeline.py
@@ -1,3 +1,6 @@
+import urllib
+import simplejson as json
+
 def get_data_fb(strategy, details, response, uid, user, *args, **kwargs):
     social = kwargs.get('social') or strategy.storage.user.get_social_auth(
         strategy.backend.name,
@@ -5,14 +8,14 @@ def get_data_fb(strategy, details, response, uid, user, *args, **kwargs):
     )
     url = None
     if strategy.backend.name == 'facebook':
-        url = 'http://graph.facebook.com/%s/picture?type=large' % response['id']
-        friends = 'https://graph.facebook.com/fql?q=SELECT%20name,uid,' \
+        photo_url = 'http://graph.facebook.com/%s/picture?type=large' % response['id']
+        friends = urllib.urlopen('https://graph.facebook.com/fql?q=SELECT%20name,uid,' \
                   'current_location.latitude,current_location.longitude' \
                   '%20FROM%20user%20WHERE%20uid%20IN%20(SELECT%20uid2%20FROM%20friend%20WHERE%20uid1%20=%20me())' \
-                  '&access_token=' % social.extra_data['access_token']
+                  '&access_token=' % social.extra_data['access_token']).read()
 
-    if url:
-        social.set_extra_data({'photo': url, 'friends': friends})
+    if photo_url:
+        social.set_extra_data({'photo': photo_url, 'friends': friends})
 
 def get_data_vk(strategy, details, response, uid, user, *args, **kwargs):
     pass

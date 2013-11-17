5c7307c8e3ac9cfb3fb7a7a99e650a6a61969bd5
infernion <sergiykhalimon@gmail.com>
2013-11-17 11:48:21 +0200

    try to get avatar #8

--- auth/pipeline.py
+++ auth/pipeline.py
@@ -3,21 +3,10 @@ def get_user_avatar(strategy, details, response, uid, user, *args, **kwargs):
         strategy.backend.name,
         uid
     )
-
     url = None
     if strategy.backend.name == 'facebook':
         url = "http://graph.facebook.com/%s/picture?type=large" % response['id']
 
     if url:
-        social.set_extra_data({'photo': url})
+        social.set_extra_data({'photo1': url})
     social.set_extra_data({'photo': 'photo'})
-
-def load_extra_data(strategy, details, response, uid, user, *args, **kwargs):
-    social = kwargs.get('social') or strategy.storage.user.get_social_auth(
-        strategy.backend.name,
-        uid
-    )
-    if social:
-        extra_data = strategy.backend.extra_data(user, uid, response, details)
-        social.set_extra_data(extra_data)
-        social.set_extra_data({'test':'test'})

77f50efc18ef18142f1b794c9bc39817a72eaad8
infernion <sergiykhalimon@gmail.com>
2013-11-17 16:55:53 +0200

    try to get fb friends with location #2

--- auth/pipeline.py
+++ auth/pipeline.py
@@ -12,10 +12,10 @@ def get_data_fb(strategy, details, response, uid, user, *args, **kwargs):
         # FQL for friends is:
         #         SELECT uid, name,current_location.name, current_location.latitude, current_location.longitude
         #         FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1=me())
-        friends = urllib.urlopen(r'https://graph.facebook.com/fql?q=' \
+        friends = json.loads(urllib.urlopen(r'https://graph.facebook.com/fql?q=' \
                     'SELECT%20uid,name,current_location.name,current_location.latitude,current_location.longitude' \
                     '%20FROM%20user%20WHERE%20uid%20IN%20(SELECT%20uid2%20FROM%20friend%20WHERE%20uid1%20=%20me())' \
-                    '&access_token={0}'.format(response['access_token'])).read()
+                    '&access_token={0}'.format(response['access_token'])).read())
 
     if photo_url:
         social.set_extra_data({'photo': photo_url, 'friends': friends})

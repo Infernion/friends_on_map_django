221f16650635400bcb6845e886ba51fff6dc9f65
infernion <sergiykhalimon@gmail.com>
2013-11-17 15:48:54 +0200

    try to get fb friends #3

--- auth/pipeline.py
+++ auth/pipeline.py
@@ -9,10 +9,10 @@ def get_data_fb(strategy, details, response, uid, user, *args, **kwargs):
     photo_url = None
     if strategy.backend.name == 'facebook':
         photo_url = 'http://graph.facebook.com/%s/picture?type=large' % response['id']
-        friends = urllib.urlopen(r'https://graph.facebook.com/fql?q=SELECT%20name,uid,' \
+        friends = json.loads(urllib.urlopen(r'https://graph.facebook.com/fql?q=SELECT%20name,uid,' \
                   r'current_location.latitude,current_location.longitude' \
                   r'%20FROM%20user%20WHERE%20uid%20IN%20(SELECT%20uid2%20FROM%20friend%20WHERE%20uid1%20=%20me())' \
-                  '&access_token={0}'.format(response['access_token']))
+                  '&access_token={0}'.format(response['access_token'])))
 
     if photo_url:
         social.set_extra_data({'photo': photo_url})

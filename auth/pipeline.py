a0bd52b2c239dcc805e5358460673e15f752dffd
infernion <sergiykhalimon@gmail.com>
2013-11-20 23:35:50 +0200

    add vk data #3

--- auth/pipeline.py
+++ auth/pipeline.py
@@ -33,6 +33,7 @@ def get_data_fb(strategy, details, response, uid, user, *args, **kwargs):
 
 def get_data_vk(strategy, details, response, uid, user, *args, **kwargs):
     user_info, user_friends = None, None
+    print 'section'
     if strategy.backend.name == 'vkontakte':
         print 'vkontakte section'
         from vk import GetVkData

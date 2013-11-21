a1b11185f77da8a3a9bb7868417fcd0b38f0629d
infernion <sergiykhalimon@gmail.com>
2013-11-21 20:52:28 +0200

    add facebook.py with GetData class #1

--- auth/pipeline.py
+++ auth/pipeline.py
@@ -10,7 +10,7 @@ logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(a
 
 
 def get_data_fb(strategy, details, response, uid, user, *args, **kwargs):
-    photo_url = None
+    photo_url, friends = None, None
     if strategy.backend.name == 'facebook':
         from urllib import quote
         from facebook import GetFacebookData
@@ -26,18 +26,13 @@ def get_data_fb(strategy, details, response, uid, user, *args, **kwargs):
         #         FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1=me())
         friends = get_data.call_api('fql', {
         'q': quote('SELECT uid, name,current_location.name, current_location.latitude, current_location.longitude FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1=me())')}, ',')
-
-        #friends = json.loads(urllib.urlopen(r'https://graph.facebook.com/fql?q=' \
-        #                                    'SELECT%20uid,name,current_location.name,current_location.latitude,current_location.longitude' \
-        #                                    '%20FROM%20user%20WHERE%20uid%20IN%20(SELECT%20uid2%20FROM%20friend%20WHERE%20uid1%20=%20me())' \
-        #                                    '&access_token={0}'.format(response['access_token'])).read())
-        #location = response['location']
-    if photo_url:
+        logging.info('user_fr', friends)
+    if photo_url and friends:
         social.set_extra_data({'photo': photo_url, 'friends': friends})
 
 
 def get_data_vk(strategy, details, response, uid, user, *args, **kwargs):
-    photo_url, user_friends = None, None
+    photo_url, friends = None, None
     if strategy.backend.name == 'vk-oauth2':
         from vk import GetVkData
 
@@ -50,10 +45,10 @@ def get_data_vk(strategy, details, response, uid, user, *args, **kwargs):
         #user_info = get_data.call_api('users.get', {'fields': 'city,country'})
         photo_url = get_data.call_api('users.get', {'fields': 'photo_rec'})
         logging.info('user_in ', user_info)
-        user_friends = get_data.call_api('friends.get', {'fields': 'uid,first_name,last_name,country,city,photo'})
-        logging.info('user_fr', user_friends)
+        friends = get_data.call_api('friends.get', {'fields': 'uid,first_name,last_name,country,city,photo'})
+        logging.info('user_fr', friends)
     if user_info and user_friends:
-        social.set_extra_data({'photo': photo_url, 'friends': user_friends})
+        social.set_extra_data({'photo': photo_url, 'friends': friends})
 
 
 

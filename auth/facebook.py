4c6ae7d12419d4fec976a8c0702c9a1585dfbfc3
infernion <sergiykhalimon@gmail.com>
2013-11-21 20:34:15 +0200

    add facebook.py with GetData class

---
+++ auth/facebook.py
@@ -0,0 +1,37 @@
+from urllib import urlencode
+import urllib2
+import json
+import time
+import logging
+
+from django.core.cache import cache as memcache
+
+class GetFacebookData(object):
+    def __init__(self, uid, token):
+        self.uid = uid
+        self.token = token
+
+    def call_api(self, method, params):
+        '''
+        This method formed QUERY with accepted parameter to VK API
+        :param cids: for show country/city with id
+        :param method: what query we do
+        :param fields: what fields we will get
+        :return: dict
+        '''
+        time.sleep(max(0.0, 0.3333 - (time.clock() - self.last_time)))
+
+        if isinstance(params, list):
+            params_list = params[:]
+        elif isinstance(params, dict):
+            params_list = params.items()
+        else:
+            params_list = [params]
+
+        params_list += [('access_token', self.token)]
+        url = 'https://graph.facebook.com/%s?%s' % (method, urlencode(params_list))
+
+        response = urllib2.urlopen(url).read()
+        self.last_time = time.clock()
+
+        return (json.loads(response))['response']  # If use get method return all item in value
--- auth/pipeline.py
+++ auth/pipeline.py
@@ -1,5 +1,3 @@
-import urllib
-import json
 import logging
 
 logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
@@ -12,42 +10,51 @@ logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(a
 
 
 def get_data_fb(strategy, details, response, uid, user, *args, **kwargs):
-    social = kwargs.get('social') or strategy.storage.user.get_social_auth(
-        strategy.backend.name,
-        uid
-    )
     photo_url = None
     if strategy.backend.name == 'facebook':
+        from urllib import quote
+        from facebook import GetFacebookData
+
+        get_data = GetFacebookData(response['id'], response['access_token'])
+        social = kwargs.get('social') or strategy.storage.user.get_social_auth(
+            strategy.backend.name,
+            uid
+        )
         photo_url = 'http://graph.facebook.com/%s/picture?type=large' % response['id']
         # FQL for friends is:
         #         SELECT uid, name,current_location.name, current_location.latitude, current_location.longitude
         #         FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1=me())
-        friends = json.loads(urllib.urlopen(r'https://graph.facebook.com/fql?q=' \
-                    'SELECT%20uid,name,current_location.name,current_location.latitude,current_location.longitude' \
-                    '%20FROM%20user%20WHERE%20uid%20IN%20(SELECT%20uid2%20FROM%20friend%20WHERE%20uid1%20=%20me())' \
-                    '&access_token={0}'.format(response['access_token'])).read())
+        friends = get_data.call_api('fql', {
+        'q': quote('SELECT uid, name,current_location.name, current_location.latitude, current_location.longitude '
+                   'FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1=me())')})
+
+        #friends = json.loads(urllib.urlopen(r'https://graph.facebook.com/fql?q=' \
+        #                                    'SELECT%20uid,name,current_location.name,current_location.latitude,current_location.longitude' \
+        #                                    '%20FROM%20user%20WHERE%20uid%20IN%20(SELECT%20uid2%20FROM%20friend%20WHERE%20uid1%20=%20me())' \
+        #                                    '&access_token={0}'.format(response['access_token'])).read())
         #location = response['location']
     if photo_url:
         social.set_extra_data({'photo': photo_url, 'friends': friends})
 
 
 def get_data_vk(strategy, details, response, uid, user, *args, **kwargs):
-    user_info, user_friends = None, None
+    photo_url, user_friends = None, None
     if strategy.backend.name == 'vk-oauth2':
-        logging.info('vk section')
         from vk import GetVkData
+
         social = kwargs.get('social') or strategy.storage.user.get_social_auth(
             strategy.backend.name,
             uid
         )
         get_data = GetVkData(response['uid'], response['access_token'])  # uid, token
         logging.info('get_data', get_data)
-        user_info = get_data.call_api('users.get', {'fields': 'city,country,photo_rec'})
+        #user_info = get_data.call_api('users.get', {'fields': 'city,country'})
+        photo_url = get_data.call_api('users.get', {'fields': 'photo_rec'})
         logging.info('user_in ', user_info)
         user_friends = get_data.call_api('friends.get', {'fields': 'uid,first_name,last_name,country,city,photo'})
         logging.info('user_fr', user_friends)
     if user_info and user_friends:
-        social.set_extra_data({'user_data': user_info, 'friends': user_friends})
+        social.set_extra_data({'photo': photo_url, 'friends': user_friends})
 
 
 

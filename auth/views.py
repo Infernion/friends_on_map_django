8cd83664edf5e8911e92c5020065fdfbdeaf6ac8
infernion <sergiykhalimon@gmail.com>
2013-11-22 03:11:12 +0200

    add photo and hiden fields #1

--- auth/views.py
+++ auth/views.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 
 def login(request):
     """Home view, displays login mechanism"""
@@ -16,7 +18,9 @@ def login(request):
 @login_required
 def done(request):
     """Login complete view, displays user data"""
+    user_model = User.objects.get(username=request.user)
     return render_to_response('done.html', {
+        'user_model': user_model,
         'user': request.user,
         'vk_uid': getattr(settings, 'SOCIAL_AUTH_VK_OAUTH2_KEY', None),
         'fb_uid': getattr(settings, 'SOCIAL_AUTH_FACEBOOK_KEY', None),

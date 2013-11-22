b86c40ea27ca3d1cd9fcb824cdb70554a8937cfc
infernion <sergiykhalimon@gmail.com>
2013-11-22 16:36:34 +0200

    fix data import bugs #3

--- auth/vk.py
+++ auth/vk.py
@@ -141,7 +141,7 @@ class GetVkData(object):
     def format_address(self, field, city_id, country_id):
         city = self.get_city(field[city_id])
         country = self.get_country(field[country_id])
-        address = '%s, %s' % (city, country)
+        address = '%s, %s' % (city, country.encode(encoding='utf-8'))
         #logging.warning(field[city_id])
         #logging.warning(city)
         #logging.warning(country)
